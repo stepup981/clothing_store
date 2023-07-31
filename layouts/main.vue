abc001a41700bb57c0b4faf54dbaee2376361111
Roman Berdnikov <berdnikov.roma@gmail.com>
2023-07-31 14:53:39 +0300

    fix sass and create layout

---
+++ layouts/main.vue
@@ -0,0 +1,18 @@
+<script>
+   export default {
+      
+   }
+</script>
+
+<template>
+   <div>
+      <div>Это и есть Хедер</div>
+      <nuxt />
+      <div>Тут Будет футер </div>
+   </div>
+</template>
+
+
+<style lang="scss">
+   
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,19 @@
-<template>
-  <Tutorial />
-</template>
-
 <script>
+import Banner from '@/components/index/Banner.vue'
+
 export default {
   name: 'IndexPage',
+  layout: 'main',
+  components: {
+    Banner
+  }
 }
 </script>
+
+<template>
+  <div>
+    <Banner/>
+  </div>
+</template>
+
+
